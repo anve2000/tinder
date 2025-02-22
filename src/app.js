265e28981c8d90971cc9c36d262aed04b1a0e7d8
Anvesha <anvesha@binmile.com>
2025-02-22 21:20:39 +0530

    refining knowledge on usage of middlewares and route handlers

--- src/app.js
+++ src/app.js
@@ -2,16 +2,37 @@ const express = require("express");
 
 const app = express(); 
 
-app.use("/test", (req, res) => {
-    res.send("test file");
-});
 
-app.use((req, res) => {
-  // request-handler
-  // Configured to send this response to every request
-  res.send("Hello from the server!");
-});
+app.get('/user',(req,res, next)=>{
+  return next();
+  // res.send('Response 1');
+},
+(req,res, next)=>{
+  // res.send('Response 2');
+  next()
+})
+
+// app.use("/test", (req, res) => {
+//     res.send("test file");
+// });
+
+// app.use((req, res) => {
+//   // request-handler
+//   // Configured to send this response to every request
+//   res.send("Hello from the server!");
+// });
+
+
+app.get('/hello',(req,res,next)=>{
+  // res.send('route hanlder2');
+  next();
+})
+
+app.get('/hello', (req,res, next)=>{
+  next();
+})
 
 app.listen(3000, () => {
   console.log("Server is listening on port 3000");
 });
+ 
